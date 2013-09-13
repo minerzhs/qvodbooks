2fe4b854365488886cef4139b6c40ac9a3525ee9
Haisu Zuo <zuohaisu@gmail.com>
2013-09-13 23:36:00 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qvodbooks::Application.config.secret_key_base = 'd476443bfe5494e8f19a2f2aaf714eb49d5ca1903c178544955a35fa580ad0f6903690ac67eae1f323e3a0d2ff342c6c5ee76e73331eb3b7fed6f2867c71d614'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Qvodbooks::Application.config.session_store :cookie_store, key: '_qvodbooks_session'
