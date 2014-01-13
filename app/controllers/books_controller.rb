74fd4baf2698f85071b3a99afba2e91c6ca34750
Haisu Zuo <zuohaisu@gmail.com>
2014-01-13 13:23:39 +0800

    add version tag filter exception show and index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
     @books = Book.all
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,6 @@
 class RecordsController < ApplicationController
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
     @records = Record.all
